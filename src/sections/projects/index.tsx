e15e689c6e7870c968b2cac2e112709876fe88a7
edersonff <edersonff_@hotmail.com.br>
2024-08-11 06:02:50 -0300

    fix: gsap horizontal scroll

--- src/sections/projects/index.tsx
+++ src/sections/projects/index.tsx
@@ -5,66 +5,88 @@ import { useLocomotiveScroll } from "react-locomotive-scroll";
 import LocomotiveScroll from "locomotive-scroll";
 
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/all";
 import { useGSAP } from "@gsap/react";
+import { useLocomotiveStore } from "@/store/locomotive";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function ProjectsSection() {
+  const locomotive = useLocomotiveStore((state) => state.locomotive);
   const [currentProject, setCurrentProject] = useState(0);
-
-  const { scroll } = useLocomotiveScroll();
 
   const container = useRef<HTMLDivElement>(null);
   const trigger = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    let ctx = gsap.context(
-      () => {
-        if (!container.current || !trigger.current) {
-          return;
-        }
+  useGSAP(() => {
+    const scrollContainer = locomotive?.el;
 
-        const tl = gsap.timeline({
-          scrollTrigger: {
-            trigger: trigger.current,
-            start: "top top",
-            end: "bottom top",
-            scrub: 0.5,
-            pin: true,
-          },
-        });
+    if (!scrollContainer || !locomotive) {
+      return;
+    }
 
-        tl.to(container.current, {
-          x: () => -(container.current?.clientWidth || 0) * projects.length,
-        });
+    ScrollTrigger.scrollerProxy(scrollContainer, {
+      scrollTop(value) {
+        const val = arguments.length
+          ? locomotive.scrollTo(value || 0, { duration: 0, disableLerp: true })
+          : locomotive.scroll.instance.scroll.y;
+
+        return val as number;
       },
-      { immediateRender: true }
-    );
+      getBoundingClientRect() {
+        return {
+          left: 0,
+          top: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
 
-    return () => ctx.revert();
-  }, []);
+      pinType: scrollContainer.style.transform ? "transform" : "fixed",
+    });
+
+    const pin = gsap.to(container.current, {
+      x: -window.innerWidth,
+      ease: "none",
+      scrollTrigger: {
+        scroller: scrollContainer,
+        trigger: trigger.current,
+        start: "top top",
+        scrub: true,
+        end: () => `+=${container.current?.offsetWidth}`,
+        pin: true,
+      },
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => {
+      locomotive.update();
+    });
+
+    ScrollTrigger.refresh();
+  }, [locomotive]);
 
   return (
     <div className="content flex-center flex-1" ref={trigger}>
-      <div ref={container} className="flex ">
-        {projects.map((project, index) => (
-          <>
-            <div key={index} className="flex-center flex-col w-[50vw]">
-              <div className="relative w-full h-[300px]"></div>
-              <div className="text-center">
-                <h3 className="text-2xl font-bold">{project.title}</h3>
-                <p>{project.description}</p>
+      <div>
+        <div ref={container} className="flex ">
+          {projects.map((project, index) => (
+            <>
+              <div key={index} className="flex-center flex-col w-[50vw]">
+                <div className="relative w-full h-[300px]"></div>
+                <div className="text-center">
+                  <h3 className="text-2xl font-bold">{project.title}</h3>
+                  <p>{project.description}</p>
+                </div>
               </div>
-            </div>
 
-            <div key={index} className="flex-center flex-col w-[50vw]">
-              <div className="relative w-full h-[300px]"></div>
-              <div className="text-center">
-                <h3 className="text-2xl font-bold">{project.title}</h3>
-                <p>{project.description}</p>
+              <div key={index} className="flex-center flex-col w-[50vw]">
+                <div className="relative w-full h-[300px]"></div>
+                <div className="text-center">
+                  <h3 className="text-2xl font-bold">{project.title}</h3>
+                  <p>{project.description}</p>
+                </div>
               </div>
-            </div>
-          </>
-        ))}
+            </>
+          ))}
+        </div>
       </div>
     </div>
   );
