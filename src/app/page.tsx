f3c60ce98c1f270ac925bbb7b293c7e274ef3339
edersonff <edersonff_@hotmail.com.br>
2024-08-22 20:28:45 -0300

    feat: horizontal texts inside second section and typing animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ import { projects } from "@/theme/projects";
 import Footer from "@/components/footer";
 import GraphSection from "@/sections/graph";
 import CommitsSections from "@/sections/commits";
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
   const currentProject = useProjectStore((state) => state.project);
@@ -52,10 +53,10 @@ export default function Home() {
         >
           <Header />
 
-          <section className="mt-[6%] mb-[2%]">
-            <div className="content flex items-center small:flex-col gap-main">
+          <section className="mt-[5%] mb-[2%]">
+            <div className="content flex big:items-center small:w-full small:flex-col gap-main">
               <div className="flex-1">
-                <h2 className="font-source-code-pro text-8xl small:text-6xl font-extrabold uppercase mb-5 text-neutral-900">
+                <h2 className="font-source-code-pro text-8xl small:text-5xl font-extrabold uppercase mb-5 text-neutral-900">
                   <span className="bg-gradient-to-b from-white to-neutral-500 text-clip">
                     WE MAKE
                   </span>
@@ -72,24 +73,24 @@ export default function Home() {
                   <Image
                     src="/images/icons/exclaimation.svg"
                     alt="Exclaimation"
-                    width={90}
-                    height={90}
+                    width={80}
+                    height={80}
                     priority
                     className="unselectable undraggable inline-block ml-0 -mt-4 transform small:scale-75"
                   />
                 </h2>
-                <p className="text-xs small:text-[10px] font-medium text-neutral-400 leading-[250%] max-w-main-5 relative z-10">
+                <p className="text-xs small:text-[10px] font-medium text-neutral-400 leading-[250%] big:max-w-main-5 relative z-10">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut
                 </p>
-                <div className="flex gap-main mt-28 relative z-10">
+                <div className="flex gap-main small:flex-col mt-[10%] relative z-10">
                   <GlitchBtn>
                     <Link
                       href="#"
                       role="button"
-                      className="flex-3 overflow-hidden max-w-main-3 bg-gradient-to-b from-primary to-secondary text-white font-bold uppercase text-sm small:text-xs text-center px-5 py-3 relative group rounded-full"
+                      className="flex-3 overflow-hidden small:w-full big:max-w-main-3 bg-gradient-to-b from-primary to-secondary text-white font-bold uppercase text-sm small:text-xs text-center px-5 py-3 relative group rounded-full"
                     >
                       Let&apos;s Code
                     </Link>
@@ -98,8 +99,9 @@ export default function Home() {
                     <Link
                       href="#"
                       role="button"
-                      className="flex-2 max-w-main-2 stroke-glass text-zinc-400 font-bold uppercase text-sm small:text-xs text-center px-5 py-3 relative group rounded-full"
+                      className="flex-2 small:w-full big:max-w-main-2 text-zinc-400 font-bold uppercase text-sm small:text-xs text-center px-5 py-3 relative group"
                     >
+                      <div className="absolute-full box-glass rounded-full group-hover:bg-white/10"></div>
                       Contact us
                     </Link>
                   </GlitchBtn>
@@ -128,8 +130,58 @@ export default function Home() {
           image="static-hero"
           className="min-h-screen flex bg-neutral-950 py-[4%]"
         >
-          <div className="content relative flex-center flex-1">
+          <div className="absolute top-0 left-0 w-full h-full flex text-[300px] small:text-[200px] flex-col justify-between overflow-hidden opacity-50">
             <div
+              data-scroll
+              data-scroll-direction="horizontal"
+              data-scroll-speed="4"
+              className="flex"
+            >
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+            </div>
+            <div
+              data-scroll
+              data-scroll-direction="horizontal"
+              data-scroll-speed="-4"
+              className="flex"
+            >
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+            </div>
+            <div
+              data-scroll
+              data-scroll-direction="horizontal"
+              data-scroll-speed="4"
+              className="flex"
+            >
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+              <h2 className="font-source-code-pro leading-[100%] text-stroke font-extrabold uppercase text-white/5">
+                PROJECTS
+              </h2>
+            </div>
+          </div>
+          <div className="content relative flex-center flex-1">
+            {/* <div
               data-scroll
               data-scroll-speed="4"
               className="flex-center relative -z-10"
@@ -143,13 +195,13 @@ export default function Home() {
                 className="unselectable undraggable inline-block relative opacity-60 z-0 "
               />
               <div className="absolute min-w-[380px] h-[0px] -z-20 shadow-[0px_0px_600px_100px_rgb(7,214,242,0.75)] mix-blend-color-dodge" />
-            </div>
+            </div> */}
 
             <Draggable>
               <div
                 data-scroll
                 data-scroll-speed="1"
-                className="absolute top-10 left-20 z-10 bg-black/40 backdrop-blur-xl rounded-xl transform small:scale-75"
+                className="absolute top-10 left-20 z-10 bg-black/40 backdrop-blur-md rounded-xl transform small:scale-75"
               >
                 <Image
                   src="/images/terminals/terminal-2.svg"
@@ -176,7 +228,7 @@ export default function Home() {
               <div
                 data-scroll
                 data-scroll-speed="2"
-                className="absolute z-20 top-[15%] left-[25%] bg-black/40 backdrop-blur-xl rounded-xl transform small:scale-75"
+                className="absolute z-20 top-[15%] left-[25%] bg-black/40 backdrop-blur-md rounded-xl transform small:scale-75"
               >
                 <Image
                   src="/images/terminals/terminal-1.svg"
@@ -187,8 +239,23 @@ export default function Home() {
                   className="opacity-40 unselectable undraggable"
                 />
                 <p className="absolute top-[30%] left-1 z-10 leading-[150%] text-sm font-source-code-pro w-full pr-5 text-primary">
-                  A vibrant testing ground to experiment with ideas, encouraging
-                  creativity and risk-taking.
+                  <TypeAnimation
+                    deletionSpeed={{
+                      type: "keyStrokeDelayInMs",
+                      value: 10,
+                    }}
+                    speed={{
+                      type: "keyStrokeDelayInMs",
+                      value: 50,
+                    }}
+                    repeat={Infinity}
+                    sequence={[
+                      "A vibrant testing ground to experiment with ideas, encouraging creativity and risk-taking.",
+                      5000,
+                      "",
+                      1000,
+                    ]}
+                  />
                 </p>
               </div>
             </Draggable>
@@ -197,7 +264,7 @@ export default function Home() {
               <div
                 data-scroll
                 data-scroll-speed="1"
-                className="absolute bottom-[0%] left-10 z-30 bg-black/40 backdrop-blur-xl rounded-xl transform small:scale-75"
+                className="absolute bottom-[0%] left-10 z-30 bg-black/40 backdrop-blur-md rounded-xl transform small:scale-75"
               >
                 <Image
                   src="/images/terminals/terminal-3.svg"
@@ -221,8 +288,24 @@ export default function Home() {
     {"\n"}|__/  |__/   |__/   |__/     |__/ \_______/   \___/  |__/  |__/
     </p>
                   <p className="leading-[150%] text-sm font-source-code-pro w-full mt-4 pr-4">
-                    Do math, machine learning, math optimization in real-time.
-                    feed Products join about
+                    <TypeAnimation
+                      deletionSpeed={{
+                        type: "keyStrokeDelayInMs",
+                        value: 10,
+                      }}
+                      speed={{
+                        type: "keyStrokeDelayInMs",
+                        value: 50,
+                      }}
+                      repeat={Infinity}
+                      sequence={[
+                        2000,
+                        "Do math, machine learning, math optimization in real-time. feed Products join about",
+                        5000,
+                        "",
+                        1000,
+                      ]}
+                    />
                   </p>
                 </div>
               </div>
@@ -232,7 +315,7 @@ export default function Home() {
               <div
                 data-scroll
                 data-scroll-speed="2"
-                className="absolute z-20 bottom-[15%] right-[0%] bg-black/40 backdrop-blur-xl rounded-xl transform small:scale-75"
+                className="absolute z-20 bottom-[15%] right-[0%] bg-black/40 backdrop-blur-md rounded-xl transform small:scale-75"
               >
                 <Image
                   src="/images/terminals/terminal-1.svg"
@@ -243,8 +326,24 @@ export default function Home() {
                   className="opacity-40 unselectable undraggable"
                 />
                 <p className="absolute top-[30%] left-1 z-10 leading-[150%] text-sm font-source-code-pro w-full pr-5 text-primary">
-                  A place to share inspiration, conduct research, build
-                  prototypes, and launch successful products.
+                  <TypeAnimation
+                    deletionSpeed={{
+                      type: "keyStrokeDelayInMs",
+                      value: 10,
+                    }}
+                    speed={{
+                      type: "keyStrokeDelayInMs",
+                      value: 50,
+                    }}
+                    repeat={Infinity}
+                    sequence={[
+                      1000,
+                      "A place to share inspiration, conduct research, build prototypes, and launch successful products.",
+                      5000,
+                      "",
+                      1000,
+                    ]}
+                  />
                 </p>
               </div>
             </Draggable>
