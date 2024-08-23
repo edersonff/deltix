8a1ce0d01086d50cdafb41e6472ed9b420fd000f
edersonff <edersonff_@hotmail.com.br>
2024-08-23 17:07:40 -0300

    fix: commits section removed because bad performance

---
+++ src/app/removed/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import LocomotiveProvider from "@/providers/locomotive";
+import React from "react";
+import Loading from "../loading";
+import Footer from "@/components/footer";
+import GraphSection from "@/sections/graph";
+import CommitsSections from "@/sections/commits";
+
+export default function Removed() {
+  return (
+    <LocomotiveProvider>
+      <Loading isComponent={true} />
+
+      <main>
+        <section
+          data-scroll-section
+          className="min-h-screen relative flex-center text-4xl"
+        >
+          <CommitsSections />
+        </section>
+
+        <section data-scroll-section>
+          <Footer />
+        </section>
+      </main>
+    </LocomotiveProvider>
+  );
+}
