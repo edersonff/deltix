ffce4f8304380ba043610ce450ff49fc53906b19
edersonff <edersonff_@hotmail.com.br>
2024-08-24 18:48:16 -0300

    feat: first loading screen and transition added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,31 +1,81 @@
 "use client";
 
-import Noise from "@/components/noise";
 import { useLoadingStore } from "@/store/loading";
-import { transition } from "@/theme/animation";
-import { motion } from "framer-motion";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useRef, useState } from "react";
 
-export default function Loading({ isComponent }: { isComponent: boolean }) {
+export default function Loading({ isComponent }: { isComponent?: boolean }) {
   const [loading, setLoading] = useLoadingStore((state) => [
     state.loading,
     state.setLoading,
   ]);
 
+  if (!loading) {
+    return <Transition isComponent={isComponent} />;
+  }
+
   return (
-    <video
-      autoPlay
-      onEnded={() => {
-        setLoading(false);
-      }}
-      muted
-      playsInline
-      className="fixed-full z-[9999999] object-cover transition-all pointer-events-none"
+    <>
+      <video
+        onEnded={() => {
+          setLoading(false);
+        }}
+        autoPlay
+        muted
+        playsInline
+        className="fixed-full z-[9999999] object-cover transition-all pointer-events-none opacity-0"
+        style={{
+          opacity: loading ? 1 : 0,
+        }}
+        src="/videos/logo.webm"
+      />
+
+      <Image
+        src="/videos/logo.webp"
+        className="fixed-full pointer-events-none z-[9999998] object-cover first-frame"
+        alt="Logo Loading"
+        width={1920}
+        height={1080}
+      />
+    </>
+  );
+}
+
+export function Transition({ isComponent }: { isComponent?: boolean }) {
+  const firstRenderRef = useRef(true);
+  const [transition, setTransition] = useLoadingStore((state) => [
+    state.transition,
+    state.setTransition,
+  ]);
+
+  if (firstRenderRef.current && !isComponent) {
+    firstRenderRef.current = false;
+
+    setTransition(true);
+  }
+
+  if (firstRenderRef.current && isComponent) {
+    firstRenderRef.current = false;
+
+    setTimeout(() => {
+      setTransition(false);
+    }, 1000);
+  }
+
+  return (
+    <div
+      className="fixed-full flex-center pointer-events-none z-[9999998] bg-[#2c3552] opacity-100 transition-opacity duration-1000"
       style={{
-        opacity: loading ? 1 : 0,
+        opacity: transition ? 1 : 0,
       }}
-      src="/videos/logo.webm"
-    />
+    >
+      <Image
+        src="/videos/loader.gif"
+        width={100}
+        height={100}
+        className="object-cover w-[400px] h-[400px]"
+        alt="Logo Loading"
+      />
+    </div>
   );
 }
--- src/store/loading/index.tsx
+++ src/store/loading/index.tsx
@@ -2,12 +2,18 @@ import { create } from "zustand";
 
 export type LoadingStore = {
   loading: boolean;
+  transition: boolean;
   setLoading: (loading: boolean) => void;
+  setTransition: (transition: boolean) => void;
 };
 
 export const useLoadingStore = create<LoadingStore>((set) => ({
   loading: true,
   setLoading: (loading) => {
     set({ loading });
+  },
+  transition: false,
+  setTransition: (transition) => {
+    set({ transition });
   },
 }));
