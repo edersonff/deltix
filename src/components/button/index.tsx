01850a2880e5c7276e129bc5557fbfbe859cb53f
edersonff <edersonff_@hotmail.com.br>
2024-08-24 19:05:44 -0300

    chore: ButtonOverlay component removed

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,35 +1,21 @@
 import React from "react";
 import { useGlitch } from "react-powerglitch";
-import { useMouse } from "@uidotdev/usehooks";
 
-export function ButtonOverlay() {
-  const [{ x, y, elementX, elementY }, ref] = useMouse<HTMLDivElement>();
-
-  return (
-    <>
-      <div className="absolute-full z-50" ref={ref} />
-      {elementX > 0 && elementY > 0 && (
-        <div
-          className="absolute w-0 h-0 top-0 left-0 aspect-square bg-white rounded-full pointer-events-none group-hover:w-[700px] group-hover:h-[700px] transition-all duration-500 transform -translate-x-1/2 -translate-y-1/2"
-          style={{
-            top: Number(elementX) + "px",
-            left: Number(elementY) + "px",
-          }}
-        />
-      )}
-    </>
-  );
-}
-
-export default function GlitchBtn({ children }: { children: React.ReactNode }) {
+export default function GlitchBtn({
+  children,
+  containerClass,
+}: {
+  children: React.ReactNode;
+  containerClass?: string;
+}) {
   const { ref } = useGlitch({
     playMode: "click",
+    createContainers: false,
   });
 
-  return React.cloneElement(children as React.ReactElement, {
-    ref,
-    style: {
-      width: "100vw",
-    },
-  });
+  return (
+    <div className={containerClass} ref={ref}>
+      {React.cloneElement(children as React.ReactElement)}
+    </div>
+  );
 }
