41d30e0cd384852220f65de5096a8dc5a84bed70
edersonff <edersonff_@hotmail.com.br>
2024-08-24 19:01:07 -0300

    perf: using css instead of motion in ProjectsBackground

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,6 +1,5 @@
 import { projects } from "@/theme/projects";
 import React from "react";
-import { motion } from "framer-motion";
 import Image from "next/image";
 import { useProjectStore } from "@/store/project";
 
@@ -8,12 +7,10 @@ export default function ProjectsBackground() {
   const currentProject = useProjectStore((state) => state.project);
 
   return projects.map((project, index) => (
-    <motion.div
+    <div
       key={index}
-      className="fixed top-0 left-0 w-[60vw] opacity-50 h-full"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: currentProject === index ? 1 : 0 }}
-      transition={{ duration: 0.5 }}
+      className="fixed top-0 left-0 w-[60vw] opacity-00 h-full transition-opacity duration-500"
+      style={{ opacity: currentProject === index ? 1 : 0 }}
     >
       <Image
         src={"/images/projects/" + project.image}
@@ -22,6 +19,6 @@ export default function ProjectsBackground() {
         className="object-cover"
       />
       <div className="absolute right-0 top-0 h-full w-full bg-gradient-to-l from-neutral-900 to-transparent"></div>
-    </motion.div>
+    </div>
   ));
 }
